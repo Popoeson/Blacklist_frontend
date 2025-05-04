4ffe4226214b7c1f743c03f1a4ca785a67de2673
Popoeson <samuelpopoola323@gmail.com>
2025-05-04 12:10:51 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 
 // MongoDB Connection
 mongoose.connect('mongodb+srv://Admin:BlacklistDatabase@blacklist-cluster.npsjdlx.mongodb.net/?retryWrites=true&w=majority&appName=Blacklist-cluster', {
